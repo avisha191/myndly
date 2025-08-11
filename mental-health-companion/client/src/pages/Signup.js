0f27c1df1301007415c2e290068e5b86d87457d8
avisha191 <164632028+avisha191@users.noreply.github.com>
2025-08-11 22:16:45 +0530

    Update Signup.js

--- mental-health-companion/client/src/pages/Signup.js
+++ mental-health-companion/client/src/pages/Signup.js
@@ -18,6 +18,8 @@ export default function Signup() {
     e.preventDefault();
     setLoading(true);
     try {
+      // Debug: see which backend URL the frontend is using
+    console.log("📡 API Base URL:", api.defaults.baseURL);
       // Signup
       await api.post("/auth/signup", form);
 
